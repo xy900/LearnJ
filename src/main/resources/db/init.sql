c26a3d8a9062f4f283021868258b10f22d6711cc
xy900 <369689958@qq.com>
2019-01-26 14:18:29 +0800

    增加字段

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,5 +1,8 @@
-create table test (
-	id int(11) PRIMARY key,
-	pid int(11),
-	name varchar(100)
-);
+DROP TABLE if EXISTS `test`;
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL COMMENT '数量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
