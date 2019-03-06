f29d1fd59ba3268dc32a81672d2a7f932956180c
xy900 <369689958@qq.com>
2019-03-06 19:35:49 +0800

    增加user表

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -11,3 +11,13 @@ CREATE TABLE `test` (
   `count` int(11) DEFAULT NULL COMMENT '数量',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE if EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_name` varchar(50) NOT NULL COMMENT '用户名',
+  `pass_wd` varchar(40) NOT NULL COMMENT '密码',
+  `pass_count` int(11) DEFAULT '0' COMMENT '登陆成功次数',
+  `fail_count` int(11) DEFAULT '0' COMMENT '登陆失败次数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
