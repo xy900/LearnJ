cc28141b3d489a522b6478e9a4e09a1fd79300fa
xy900 <369689958@qq.com>
2019-03-16 22:27:29 +0800

    完善zookeeper测试；解决watcher失效的问题

--- src/test/java/com/test/zk/ZkClient.java
+++ src/test/java/com/test/zk/ZkClient.java
@@ -12,7 +12,14 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
 
 /**
- * ZooKeeper
+ * ZooKeeper的使用
+ * 
+ * <p>getData,exists和getChildren可以注册watcher; 
+ * <li>一次注册只能监听一次
+ * <li>对于NodeCreated 通过exists API设置 
+ * <li>对于NodeDeleted 通过exists 和 getData()设置 
+ * <li>对于NodeDataChanged 通过exists或getData设置 
+ * <li>对于NodeChildrenChanged 通过getChildren设置
  * @author XieYu
  */
 public class ZkClient {
@@ -33,7 +40,7 @@ public class ZkClient {
 						public void process(WatchedEvent event) {
 								System.out.println("\n>>>Watcher:"+event);
 								if (event.getState() == KeeperState.SyncConnected) { // 进行连接
-									System.out.println("\n>>>进行连接");
+									System.out.println("\n>>>已经连接");
 									countDownLatch.countDown();//计数器减一, 唤醒等待的线程
 								}
 								if (event.getState() == KeeperState.Disconnected) {
@@ -57,7 +64,7 @@ public class ZkClient {
 			//getSessionPasswd获取会话秘密
 			System.out.println("\n>>>sessionPasswd:" + new String(zk.getSessionPasswd()));
 			
-			//创建节点并设置默认的监听器
+			//创建节点并设置默认的监听器并进行监听;只能监听该节点的NodeCreated类型
 			if (zk.exists("/zktest", true) == null) {
 				System.out.println("\n>>>create: " 
 					+ zk.create("/zktest", "zktest".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT));
@@ -67,12 +74,15 @@ public class ZkClient {
 			//getData()获取date信息
 			System.out.println("\n>>>data(/zktest):" + new String(zk.getData("/zktest", null, null)));
 			//不存在则抛异常
-			System.out.println("\n>>>data(/):" + new String(zk.getData("/", null, null)));
+			System.out.println("\n>>>data(/):" + new String(zk.getData("/", false, null)));
 			
 			//create创建一个临时顺序节点: create /zktest/create 测试 world:anyone:cdrwa
-			System.out.println("\n>>>create: "
-					+ zk.create("/zktest/create", "test".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL));
-			
+			//通过exist操作监听NodeCreated类型的事件 (因为是临时节点,所以事先不知道路径, 无法监听NodeCreated类型的事件)
+			String ephemeralPath = "";
+			if (zk.exists("/zktest/create", true) == null) {
+				System.out.println("\n>>>create: "
+						+ (ephemeralPath=zk.create("/zktest/create", "test".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL)));
+			}
 			//获取children
 			List<String> children = zk.getChildren("/", true);
 			if (children != null) {
@@ -81,6 +91,13 @@ public class ZkClient {
 					System.out.println("child:" + child);
 				}
 			}
+			
+			//删除临时节点并注册一个通知
+			System.out.println(">>>开始删除");
+			int ephemeralVersion = zk.exists(ephemeralPath, true).getVersion();
+			zk.delete(ephemeralPath, ephemeralVersion);
+
+			zk.close();
 		} catch (IOException e) {
 			System.out.println("\n>>>IOException!");
 			e.printStackTrace();
